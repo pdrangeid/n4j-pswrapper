ec70a045435b8165b112d801e5d9d0b727557176
pdrangeid <44095666+pdrangeid@users.noreply.github.com>
2019-05-29 11:42:01 -0500

    updated sharedfunc

--- bg-sharedfunctions.ps1
+++ bg-sharedfunctions.ps1
@@ -49,6 +49,11 @@ Function LogError($e,[String]$mymsg,[String]$section){
       $msg += "`n" + $e.Message
       }
     $warningmessage=$($section)+" - "+$($mymsg)+" - "+$($msg)
+    if ($warningmessage -like "*Failed to connect to server*") {
+        Write-Warning "Failed to connect to Neo4j server.  Aborting..."
+        sendto-eventlog -message $warningmessage -entrytype "Error"
+        BREAK
+    }
     Write-Warning $warningmessage
     sendto-eventlog -message $warningmessage -entrytype "Error"
     #BREAK
@@ -130,6 +135,79 @@ Function Get-SecurePassword([String]$pwpath,[String]$RegValName){
     Return $UnsecurePassword
     # End of Function
 }
+Function Set-SecurePassword([String]$pwpath,[String]$UsrValName,[String]$PWValName,[String]$theusername,[String]$UnsecurePW){
+    Set-ItemProperty -Path $pwpath -Name $RegValName -Value $theusername -Force #| Out-Null
+    $secure = ConvertTo-SecureString $UnsecurePW -force -asPlainText 
+    $bytes = ConvertFrom-SecureString $secure
+    Set-ItemProperty -Path $pwpath -Name $PWValName	-Value $bytes -Force #| Out-Null
+    Remove-Variable -name unsecurepw | Out-Null
+    Remove-Variable -name secure | Out-Null
+    Remove-Variable -name bytes | Out-Null
+}# End Function Set-SecurePassword
+Function Get-Set-Credential {
+    param(
+        [Parameter(Position = 0, Mandatory = $true)]
+        [String]$CredName
+        ,
+        [Parameter(Position = 1, Mandatory = $true)]
+        [String]$RegPath
+        ,
+        [Parameter(Position = 2, Mandatory = $true)]
+        [String]$UserValName
+        ,
+        [Parameter(Position = 3, Mandatory = $true)]
+        [String]$PWValName
+        ,
+        [Parameter(Position = 4, Mandatory = $false)]
+        [Boolean]$Promptpwchange
+        ,
+        [Parameter(Position = 5, Mandatory = $false)]
+        [String]$DefUserName
+        ,
+        [Parameter(Position = 6, Mandatory = $false)]
+        [String]$DialogPrompt          
+        ) 
+    
+process{
+    $defval = if ($DefUserName -eq $null) { "User" } else { $DefUserName }
+    $PWUIDialog = if ($DialogPrompt -eq $null) { "Enter your credentials." } else { $DialogPrompt }
+    #First see if we already have credentials stored in the registry
+    #$RegPath=$($RegPath+"\")
+    if ((Test-RegistryValue $RegPath $UserValName) -and (Test-RegistryValue $RegPath $PWValName)){
+        if ($Promptpwchange -eq $false){return $true}
+        $intsetacct=-1
+        if (YesorNo "Would you like to update the credentials used for [$CredName]?" "Update Credentials") {
+        $intsetacct=1
+        }# End YesorNo
+    } # End if (username AND password stored in registry)
+    write-host "intsetacct is $intsetacct"
+    if ($intsetacct -eq -1){
+        $credUser = Ver-RegistryValue -RegPath $Path -Name $UserValName -DefValue $defval
+        $credPW = Get-SecurePassword $RegPath $PWValName 
+        }
+    if ($intsetacct -ne -1){
+        $credUser = Ver-RegistryValue -RegPath $RegPath -Name $UserValName -DefValue $defval
+        write-host "about to get-cred... $PWUIDialog" 
+        #$cred = Get-Credential -Credential $defval -Message $($PWUIDialog) -Title $("$CredName credential request")
+        $cred = $host.ui.PromptForCredential("$CredName credential request", $PWUIDialog, $defval,"")
+        $justuser=$cred.username
+        $password = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($cred.password))
+        if (AmINull $($password.trim()) -eq $true) {
+        Write-Host "$CredName user password cannot be blank."
+        return $false
+        } # End if provided password was blank/null
+        } # End if $intsetacct -ne -1
+    if ($intsetacct -ne -1){
+        # -1 means we already validated the registry contains the user/password, and the user doesn't want to change them
+        Set-ItemProperty -Path $path -Name $UserValName	-Value $credUser -Force #| Out-Null
+        $secure = ConvertTo-SecureString $password -force -asPlainText 
+        $bytes = ConvertFrom-SecureString $secure
+        Set-ItemProperty -Path $path -Name $PWValName -Value $bytes -Force #| Out-Null
+        }
+        
+        return $true
+}#End Process
+}#End Function Get-Set-Credential
 
 Function AddRegPath([String]$regpath){
     $testpathresult = Test-Path -Path $regpath
@@ -170,24 +248,24 @@ function New-TemporaryDirectory {
     [string] $name = [System.Guid]::NewGuid()
     New-Item -ItemType Directory -Path (Join-Path $parent $name)
 }
-function GetAPIKEY{
-    if (Test-RegistryValue $($Path+$tenantdomain) "APIKey"){
-    $APIKey = Get-SecurePassword $($Path+$tenantdomain) "APIKey"
+
+function GetKEY([String]$pwpath,[String]$RegValName,[String]$UIPrompt){
+    if (Test-RegistryValue $($pwpath) $RegValName){
+    $GUID = Get-SecurePassword $($pwpath) $RegValName
     }
-    
+        
     # No Key, so we need to prompt for input
-    if ([string]::IsNullOrEmpty($APIKey))  {
-    Write-Host "Requesting user to supply APIKey"
-    #$password= Read-Host -assecurestring "Enter GUID for $tenantdomain"
-    $password=$Host.ui.PromptForCredential("","Enter GUID for $tenantdomain","APIKey","")
+    if ([string]::IsNullOrEmpty($GUID))  {
+    Write-Host "Requesting user to supply $RegValName `n $UIPrompt"
+    $password=$Host.ui.PromptForCredential("UID Key Request",$UIPrompt,$RegValName,"")
     if ([string]::IsNullOrEmpty($password.password)) {
-    Write-Host "No APIKey provided or dialog cancelled.   Exiting script..."
+    Write-Host "No $RegValName provided or dialog cancelled.   Exiting script..."
     BREAK
     }
     $SecureString = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password.password))
 $secure = ConvertTo-SecureString $SecureString -force -asPlainText 
 $bytes = ConvertFrom-SecureString $secure
-Ver-RegistryValue -RegPath $($Path+$tenantdomain) -Name "APIKey" -DefValue $bytes | Out-Null
+Ver-RegistryValue -RegPath $($Path+$tenantdomain) -Name $RegValName -DefValue $bytes | Out-Null
 Remove-Variable bytes
 Remove-Variable password
 Remove-Variable secure
@@ -197,16 +275,16 @@ Remove-Variable securestring
 
 Try
 {
-$APIKey = Get-SecurePassword $($Path+$tenantdomain) "APIKey"
+$GUID = Get-SecurePassword $($pwpath) $RegValName
 }
 Catch
 {
 $ErrorMessage = $_.Exception.Message
     $FailedItem = $_.Exception.ItemName
-    Write-Host "Retrieve GUID from registry Failed! We failed to read GUID from $($Path+$tenantdomain). The error message was '$ErrorMessage'  It is likely that you are not running this script as the original user who saved the secure tenant GUID."
+    Write-Host "Retrieve key from registry Failed! We failed to read key from $($pwpath). The error message was '$ErrorMessage'  It is likely that you are not running this script as the original user who saved the secure key value."
     Break
 }
-return $APIKey
+return $GUID
 # End of Function
 }
 
